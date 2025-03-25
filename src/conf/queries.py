403f2813c318e1dbfa401f8d16f1462db6175735
Anand Parthasarathy <anandp504@gmail.com>
2025-03-25 08:50:59 +0530

    Change flowvision extraction table name

--- src/conf/queries.py
+++ src/conf/queries.py
@@ -1,15 +1,15 @@
 store_request = """
-    INSERT INTO metadata_store(request_id, image_url, image_id, metadata, request_timestamp)
+    INSERT INTO flowvision_extraction_data(request_id, image_url, image_id, metadata, request_timestamp)
     VALUES(:request_id, :image_url, :image_id, :metadata, :request_timestamp)
 """
 store_response = """
-    UPDATE metadata_store
+    UPDATE flowvision_extraction_data
     SET (meter_reading_status, meter_reading, correlation_id, response_timestamp) = 
     (:meter_reading_status, :meter_reading, :correlation_id, :response_timestamp)
     WHERE request_id = (:request_id)::VARCHAR
 """
 store_feedback = """
-    UPDATE metadata_store
+    UPDATE flowvision_extraction_data
     SET (extracted_reading_accurate, actual_reading, feedback_timestamp) = 
     (:extracted_reading_accurate, :actual_reading, :feedback_timestamp)
     WHERE correlation_id = (:correlation_id)::VARCHAR
